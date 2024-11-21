91a3b8dabd3cd7f62ce40ef1e22087d780d626ed
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-11-20 20:42:57 -0600

    ReadingStringFromConsole Commit

--- src/ReadingStringFromConsole.java
+++ src/ReadingStringFromConsole.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+//Read String from Console Input
 public class ReadingStringFromConsole {
+    public void systemInDemo(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three words separated by spaces: ");
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is " + s3);
+
+    }
+    //ReadingStringFromConsole
+    public void ReadingStringFromConsole1(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class main {
+    public static void main(String[] args) {
+        ReadingStringFromConsole input = new ReadingStringFromConsole();
+        input.systemInDemo();
+        input.ReadingStringFromConsole1();
+    }
 }
